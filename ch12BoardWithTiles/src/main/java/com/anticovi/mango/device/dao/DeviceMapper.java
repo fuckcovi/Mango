8264bf557b474f2a59625493d6b2e64cc9d73007
eunho <eunho@joeunho>
2017-08-29 16:50:49 +0900

    이벤트

--- ch12BoardWithTiles/src/main/java/com/anticovi/mango/device/dao/DeviceMapper.java
+++ ch12BoardWithTiles/src/main/java/com/anticovi/mango/device/dao/DeviceMapper.java
@@ -12,9 +12,9 @@ import com.anticovi.mango.device.domain.DeviceInfoCommand;
 public interface DeviceMapper {
 	@Select("SELECT * FROM device a FULL OUTER JOIN deviceinfo b ON a.d_seq=b.d_seq")	// 사이트에 등록된 기기리스트
 	public List<DeviceCommand> allDeviceList();
-	@Select("SELECT * FROM (SELECT * FROM device WHERE d_seq=#{0.d_seq})a, deviceinfo b WHERE a.d_seq=b.d_seq AND di_seq=#{1.di_seq}") // 해당 기기 상세정보 확인
+	@Select("SELECT * FROM (SELECT * FROM device WHERE d_seq=#{d_seq})a, deviceinfo b WHERE a.d_seq=b.d_seq AND di_seq=#{1.di_seq}") // 해당 기기 상세정보 확인
 	public DeviceCommand deviceInfo(Integer d_seq, Integer di_seq);
-	@Select("SELECT * FROM deviceinfo WHERE d_seq=#{0.d_seq},di_seq=#{1.di_seq}")
+	@Select("SELECT * FROM deviceinfo WHERE d_seq=#{d_seq},di_seq=#{di_seq}")
 	public DeviceInfoCommand selectDeviceInfo(Integer d_seq,Integer di_seq);
 	@Insert("INSERT INTO device(d_seq,d_name,d_modelnum,d_regdate,d_type,d_explain) VALUES(device_seq.nextval,#{d_name},#{d_modelnum},#{d_regdate, jdbcType=DATE},#{d_type},#{d_explain})")
 	public void insertDevice(DeviceCommand device);
